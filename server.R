008ccb7064c8aa8d052383f76177a7d529567fd4
Haseeb Mushtaq <f200316@cfd.nu.edu.pk>
2022-12-03 21:49:46 +0500

    Statistical analysis and Graphical representation of data

--- server.R
+++ server.R
@@ -3,14 +3,374 @@ library(shinydashboard)
 library(plotly)
 library(datasets)
 
+
+
 shinyServer(function(input,output,session){
   ##for display water_potability dataset
   output$mydatatable <- renderDataTable({
-    water_potability
+    project
   })
   
   output$plot1 <- renderPlotly({
-    plot_ly(data = water_potability,x=~ph,type = "histogram")
+    plot_ly(data =project,x=~ph,type = "histogram",color = "red")
+  })
+   output$plot2 <- renderPlotly({
+     plot_ly(data = project,x=~Hardness,type = "histogram",color = "red")
+   })
+   output$plot3 <- renderPlotly({
+     plot_ly(data = project,x=~Solids,type = "histogram",color = "red")
+   })
+   
+   output$plot4 <- renderPlotly({
+     plot_ly(data = project,x=~Chloramines,type = "histogram",color = "red")
+   })
+   
+   output$plot5 <- renderPlotly({
+     plot_ly(data = project,x=~Sulfate,type = "histogram",color = "red")
+   })
+   
+   output$plot6 <- renderPlotly({
+     plot_ly(data = project,x=~Conductivity,type = "histogram",color = "red")
+   })
+   
+   output$plot7 <- renderPlotly({
+     plot_ly(data = project,x=~Organic_carbon,type = "histogram",color = "red")
+   })
+   
+   output$plot8 <- renderPlotly({
+     plot_ly(data = project,x=~Trihalomethanes,type = "histogram",color = "red")
+   })
+   
+   output$plot9 <- renderPlotly({
+     plot_ly(data = project,x=~Turbidity,type = "histogram",color = "red")
+   })
+   output$plot10 <- renderPlotly({
+     plot_ly(data = project,x=~Potability,type = "histogram",color = "red")
+   })
+   
+   
+   
+   
+   
+ 
+  #Statistical Analaysis of PH
+  
+  
+  output$mPH <- renderInfoBox({
+    infoBox(title = tags$b("mPH"),
+            value = mean(project$ph),
+            subtitle = "Mean of PH",fill = T,color = "green"
+            )
+  })
+  output$mH <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$ph),
+            subtitle = "Medain of PH",fill = T,color = "green"
+    )
+  })
+  output$mH1 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$ph),
+            subtitle = "MODE of PH",fill = T,color = "green"
+    )
+  })
+  output$mH2 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = sd(project$ph),
+            subtitle = "Standard Deviation PH",fill = T,color = "green"
+    )
+  })
+  output$mH3 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = var(project$ph),
+            subtitle = "Variance of PH",fill = T,color = "green"
+    )
+  })
+  
+  
+  #Statistical Analaysis of Hardness
+  output$H <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Hardness),
+            subtitle = "Mean of Haedness",fill = T,color = "green"
+    )
+  })
+  output$H1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Hardness),
+            subtitle = "Medain of Hardness",fill = T,color = "green"
+    )
+  })
+  output$H2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Hardness),
+            subtitle = "MODE of Hardness",fill = T,color = "green"
+    )
+  })
+  output$H3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Hardness),
+            subtitle = "Standard Deviation of Hardness",color = "green",fill = T
+    )
+  })
+  output$H4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Hardness),
+            subtitle = "Variance of Hardness",color = "green",fill = T
+    )
+  })
+  
+  
+  
+  
+  #Statistical Analysis of SOlids
+  
+  output$S <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Solids),
+            subtitle = "Mean of PH",fill = T,color = "green"
+    )
+  })
+  output$S1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Solids),
+            subtitle = "Medain of Solids",fill = T,color = "green"
+    )
+  })
+  output$S2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Solids),
+            subtitle = "MODE of Solids",fill = T,color = "green"
+    )
+  })
+  output$S3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Solids),
+            subtitle = "Standard Deviation of Solids",color = "green",fill = T
+    )
+  })
+  output$S4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Solids),
+            subtitle = "Variance of SOlids",color = "green",fill = T
+    )
+  })
+  
+  
+  
+  
+  
+  
+  
+  #Statistical Analysis of Chloromines
+  
+  output$C <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Chloramines),
+            subtitle = "Mean of Choloramines",fill = T,color = "green"
+    )
+  })
+  output$C1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Chloramines),
+            subtitle = "Medain of Choloramines",fill = T,color = "green"
+    )
+  })
+  output$C2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Chloramines),
+            subtitle = "MODE of Choloramines",fill = T,color = "green"
+    )
+  })
+  output$C3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Chloramines),
+            subtitle = "Standard Deviation of Chloramines",color = "green",fill = T
+    )
+  })
+  output$C4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Chloramines),
+            subtitle = "Variance of Chloramines",color = "green",fill = T
+    )
+  })
+  
+  
+  
+  
+  #Statistical Analysis of Sulfate
+  
+  output$SU <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Sulfate),
+            subtitle = "Mean of Sulfate",fill = T,color = "green"
+    )
+  })
+  output$SU1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Sulfate),
+            subtitle = "Medain of Sulfate",fill = T,color = "green"
+    )
+  })
+  output$SU2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Sulfate),
+            subtitle = "MODE of Sulfate",fill = T,color = "green"
+    )
+  })
+  output$SU3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Sulfate),
+            subtitle = "Standard Deviation of Sulfate",color = "green",fill = T
+    )
+  })
+  output$SU4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Sulfate),
+            subtitle = "Variance of SUlfate",color = "green",fill = T
+    )
+  })
+  
+  
+  
+  #Statistical Analysis of Conductivity
+  
+  output$CU <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Conductivity),
+            subtitle = "Mean of Conductivity",fill = T,color = "green"
+    )
+  })
+  output$CU <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Conductivity),
+            subtitle = "Medain of Conductivity",fill = T,color = "green"
+    )
+  })
+  output$CU2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Conductivity),
+            subtitle = "MODE of Conductivity",fill = T,color = "green"
+    )
+  })
+  
+  output$CU3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Conductivity),
+            subtitle = "Standard Deviation of Conductivity",color = "green",fill = T
+    )
+  })
+  output$CU4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Conductivity),
+            subtitle = "Variance of COnductvity",color = "green",fill = T
+    )
+  })
+  
+  
+  #Statistical Analysis of Organic Carbon
+  
+  output$O <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Organic_carbon),
+            subtitle = "Mean of Organic Carbon",fill = T,color = "green"
+    )
+  })
+  output$O1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Organic_carbon),
+            subtitle = "Medain of Organic Carbon",fill = T,color = "green"
+    )
+  })
+  output$O2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Organic_carbon),
+            subtitle = "MODE of Organic Carbon",fill = T,color = "green"
+    )
+  })
+  
+  output$O3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Organic_carbon),
+            subtitle = "Standard Deviation of Organic Carban",color = "green",fill = T
+    )
+  })
+  output$O4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Organic_carbon),
+            subtitle = "Variance of organic_carbon",color = "green",fill = T
+    )
+  })
+  
+  
+  #Statistical Analysis of Trihalomethene
+  
+  output$T <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Trihalomethanes),
+            subtitle = "Mean of Trihalomethanes",fill = T,color = "green"
+    )
+  })
+  output$T1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Trihalomethanes),
+            subtitle = "Medain of Trihalomethanes",fill = T,color = "green"
+    )
+  })
+  output$T2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Trihalomethanes),
+            subtitle = "MODE of Trihalomethanes",fill = T,color = "green"
+    )
+    
+  })
+  
+  output$T3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Trihalomethanes),
+            subtitle = "Standard Deviation of Trihalometanes",color = "green",fill = T
+    )
+  })
+  output$T4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Trihalomethanes),
+            subtitle = "Variance of Trihalomethanes",color = "green",fill = T
+    )
+  })
+  
+  
+  #Statistical Analysis of Turbidity
+  
+  output$TU <- renderInfoBox({
+    infoBox(title = tags$b("mean"),
+            value = mean(project$Turbidity),
+            subtitle = "Mean of Turbidity",fill = T,color = "green"
+    )
+  })
+  output$TU1 <- renderInfoBox({
+    infoBox(title = tags$b("Median"),
+            value = median(project$Turbidity),
+            subtitle = "Medain of Turbidity",fill = T,color = "green"
+    )
+  })
+  output$TU2 <- renderInfoBox({
+    infoBox(title = tags$b("MODE"),
+            value = mode(project$Turbidity),
+            subtitle = "MODE of Turbidity",fill = T,color = "green"
+    )
+  })
+  output$TU3 <-renderInfoBox({
+    infoBox(title = tags$b("Stadard Deviation"),
+            value = sd(project$Turbidity),
+            subtitle = "Standard Deviation of Turbidity",color = "green",fill = T
+    )
+  })
+  output$TU4 <-renderInfoBox({
+    infoBox(title = tags$b("Variance"),
+            value = var(project$Turbidity),
+            subtitle = "Variance of Turbidity",color = "green",fill = T
+    )
   })
   
 })
+
--- ui.R
+++ ui.R
@@ -24,3 +24,237 @@ shinyUI(
     )
   )
 )
+library(shiny)
+library(shinydashboard)
+library(plotly)
+library(datasets)
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "Probability Project",titleWidth = 500),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Data",tabName = "data",icon = icon("database")),
+        menuItem("Graphical Representation",tabName = "box",
+                 menuSubItem("Histogram of Ph",tabName = "box"),
+                 menuSubItem("Histogram of Hardness",tabName = "h"),
+                 menuSubItem("Histogram of Solids",tabName = "s"),
+                 menuSubItem("Histogram of Chloromines",tabName = "chloromines"),
+                 menuSubItem("Histogram of Sulfate",tabName = "sulfate"),
+                 menuSubItem("Histogram of Condunctivity",tabName = "conductivity"),
+                 menuSubItem("Histogram of Organic Carbon",tabName = "organic"),
+                 menuSubItem("Histogram of Trihelomethane",tabName = "trihelo"),
+                 menuSubItem("Histogram of Turbidity",tabName = "turbidity"),
+                 menuSubItem("Histogram of Potability",tabName = "potability")
+                 
+                 ),
+        
+        menuItem("Statistical Analysis Of PH",tabName = "mean",
+                 menuSubItem(" MEAN",tabName = "mPH"),
+                 menuSubItem(" MEDIAN",tabName = "mH"),
+                 menuSubItem(" MODE",tabName = "mH1"),
+                 menuSubItem("Standard Deviation",tabName = "mH2"),
+                 menuSubItem("Variance",tabName = "mH3")
+                 
+                 ),
+        
+        menuItem("Statistical Analysis Of Hardness",tabName = "Hardness",
+                 menuSubItem(" MEAN",tabName = "H"),
+                 menuSubItem(" MEDIAN",tabName = "H1"),
+                 menuSubItem(" MODE",tabName = "H2"),
+                 menuSubItem("Standard Deviation",tabName = "H3"),
+                 menuSubItem("Variance",tabName = "H4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Solids",tabName = "Solids",
+                 menuSubItem(" MEAN",tabName = "S"),
+                 menuSubItem(" MEDIAN",tabName = "S1"),
+                 menuSubItem(" MODE",tabName = "S2"),
+                 menuSubItem("Standard Deviation",tabName = "S3"),
+                 menuSubItem("Variance",tabName = "S4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Chloromins",tabName = "Chloromins",
+                 menuSubItem(" MEAN",tabName = "C"),
+                 menuSubItem(" MEDIAN",tabName = "C1"),
+                 menuSubItem(" MODE",tabName = "C2"),
+                 menuSubItem("Standard Deviation",tabName = "C3"),
+                 menuSubItem("Variance",tabName = "C4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Sulfate",tabName = "Sulfate",
+                 menuSubItem(" MEAN",tabName = "SU"),
+                 menuSubItem(" MEDIAN",tabName = "SU1"),
+                 menuSubItem(" MODE",tabName = "SU2"),
+                 menuSubItem("Standard Deviation",tabName = "S3"),
+                 menuSubItem("Variance",tabName = "S4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Hardness",tabName = "conductivity",
+                 menuSubItem(" MEAN",tabName = "CO"),
+                 menuSubItem(" MEDIAN",tabName = "CO1"),
+                 menuSubItem(" MODE",tabName = "CO2"),
+                 menuSubItem("Standard Deviation",tabName = "CO3"),
+                 menuSubItem("Variance",tabName = "CO4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Hardness",tabName = "Organic",
+                 menuSubItem(" MEAN",tabName = "O"),
+                 menuSubItem(" MEDIAN",tabName = "O1"),
+                 menuSubItem(" MODE",tabName = "O2"),
+                 menuSubItem("Standard Deviation",tabName = "O3"),
+                 menuSubItem("Variance",tabName = "O4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Hardness",tabName = "Trihelomethane",
+                 menuSubItem(" MEAN",tabName = "T"),
+                 menuSubItem(" MEDIAN",tabName = "T1"),
+                 menuSubItem(" MODE",tabName = "T2"),
+                 menuSubItem("Standard Deviation",tabName = "T3"),
+                 menuSubItem("Variance",tabName = "T4")
+                 
+        ),
+        
+        menuItem("Statistical Analysis Of Hardness",tabName = "Turbidity",
+                 menuSubItem(" MEAN",tabName = "TU"),
+                 menuSubItem(" MEDIAN",tabName = "TU1"),
+                 menuSubItem(" MODE",tabName = "TU2"),
+                 menuSubItem("Standard Deviation",tabName = "TU3"),
+                 menuSubItem("Variance",tabName = "TU4")
+                 
+        )
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      )
+      
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "data",dataTableOutput("mydatatable")),
+        tabItem(tabName = "box",
+                fluidRow(box(title = "Histogram of PH",plotlyOutput("plot1",height = 500))),
+              
+        ),
+        tabItem(tabName = "h",
+              
+                fluidRow(box(title = "Histogram of Hardness",plotlyOutput("plot2",height = 500)))
+        ),
+        tabItem(tabName = "s",
+                
+                fluidRow(box(title = "Histogram of Solids",plotlyOutput("plot3",height = 500)))
+        ),
+        tabItem(tabName = "chloromines",
+                
+                fluidRow(box(title = "Histogram of Chloromies",plotlyOutput("plot4",height = 500)))
+        ),
+        tabItem(tabName = "sulfate",
+                
+                fluidRow(box(title = "Histogram of Sulfate",plotlyOutput("plot5",height = 500)))
+        ),
+        tabItem(tabName = "conductivity",
+                
+                fluidRow(box(title = "Histogram of Conductivity",plotlyOutput("plot6",height = 500)))
+        ),
+        tabItem(tabName = "organic",
+                
+                fluidRow(box(title = "Histogram of Organic Carbon",plotlyOutput("plot7",height = 500)))
+        ),
+        tabItem(tabName = "trihelo",
+                
+                fluidRow(box(title = "Histogram of Trihelo",plotlyOutput("plot8",height = 500)))
+        ),
+        tabItem(tabName = "turbidity",
+                
+                fluidRow(box(title = "Histogram of turbidity",plotlyOutput("plot9",height = 500)))
+        ),
+        tabItem(tabName = "potability",
+                
+                fluidRow(box(title = "Histogram of potability",plotlyOutput("plot10",height = 500)))
+        ),
+        
+      #Staistical Analysis of PH
+        
+        tabItem(tabName = "mPH",fluidRow(infoBoxOutput("mPH",width = 4))),
+        tabItem(tabName = "mH",fluidRow(infoBoxOutput("mH",width = 4))),
+        tabItem(tabName = "mH1",fluidRow(infoBoxOutput("mH1",width = 4))),
+        tabItem(tabName = "mH2",fluidRow(infoBoxOutput("mH2",width = 4))),
+        tabItem(tabName = "mH3",fluidRow(infoBoxOutput("mH3",width = 4))),
+      
+      
+      # Statistical Analysis of Hardness
+      tabItem(tabName = "H",fluidRow(infoBoxOutput("H",width = 4))),
+      tabItem(tabName = "H1",fluidRow(infoBoxOutput("H1",width = 4))),
+      tabItem(tabName = "H2",fluidRow(infoBoxOutput("H2",width = 4))),
+      tabItem(tabName = "H3",fluidRow(infoBoxOutput("H3",width = 4))),
+      tabItem(tabName = "H4",fluidRow(infoBoxOutput("H4",width = 4))),
+      
+      #Statistical Analysis of Solids
+      tabItem(tabName = "S",fluidRow(infoBoxOutput("S",width = 4))),
+      tabItem(tabName = "S1",fluidRow(infoBoxOutput("S1",width = 4))),
+      tabItem(tabName = "S2",fluidRow(infoBoxOutput("S2",width = 4))),
+      tabItem(tabName = "S3",fluidRow(infoBoxOutput("S3",width = 4))),
+      tabItem(tabName = "S4",fluidRow(infoBoxOutput("S4",width = 4))),
+      
+      #Statistical Analysis of Chloromines
+      tabItem(tabName = "C",fluidRow(infoBoxOutput("C",width = 4))),
+      tabItem(tabName = "C1",fluidRow(infoBoxOutput("C1",width = 4))),
+      tabItem(tabName = "C2",fluidRow(infoBoxOutput("C2",width = 4))),
+      tabItem(tabName = "C3",fluidRow(infoBoxOutput("C1",width = 4))),
+      tabItem(tabName = "C4",fluidRow(infoBoxOutput("C2",width = 4))),
+      
+      #Statistical Analysis of Sulfate
+      tabItem(tabName = "SU",fluidRow(infoBoxOutput("SU",width = 4))),
+      tabItem(tabName = "SU1",fluidRow(infoBoxOutput("SU1",width = 4))),
+      tabItem(tabName = "SU2",fluidRow(infoBoxOutput("SU2",width = 4))),
+      tabItem(tabName = "SU3",fluidRow(infoBoxOutput("SU1",width = 4))),
+      tabItem(tabName = "SU4",fluidRow(infoBoxOutput("SU2",width = 4))),
+      
+      # Staistical Analysis of Conductivity
+      tabItem(tabName = "CU",fluidRow(infoBoxOutput("CU",width = 4))),
+      tabItem(tabName = "CU1",fluidRow(infoBoxOutput("CU1",width = 4))),
+      tabItem(tabName = "CU2",fluidRow(infoBoxOutput("CU2",width = 4))),
+      tabItem(tabName = "CU3",fluidRow(infoBoxOutput("CU1",width = 4))),
+      tabItem(tabName = "CU4",fluidRow(infoBoxOutput("CU2",width = 4))),
+      
+      #Statistical Analysis of Organic Carbon
+      tabItem(tabName = "O",fluidRow(infoBoxOutput("O",width = 4))),
+      tabItem(tabName = "O1",fluidRow(infoBoxOutput("O1",width = 4))),
+      tabItem(tabName = "O2",fluidRow(infoBoxOutput("O2",width = 4))),
+      tabItem(tabName = "O3",fluidRow(infoBoxOutput("O1",width = 4))),
+      tabItem(tabName = "O4",fluidRow(infoBoxOutput("O2",width = 4))),
+      
+      #Statistical Analysis of Trihelomethane
+      tabItem(tabName = "T",fluidRow(infoBoxOutput("T",width = 4))),
+      tabItem(tabName = "T1",fluidRow(infoBoxOutput("T1",width = 4))),
+      tabItem(tabName = "T2",fluidRow(infoBoxOutput("T2",width = 4))),
+      tabItem(tabName = "T3",fluidRow(infoBoxOutput("T1",width = 4))),
+      tabItem(tabName = "T4",fluidRow(infoBoxOutput("T2",width = 4))),
+      
+      #Statistical Analysis of Turbidity
+      tabItem(tabName = "TU",fluidRow(infoBoxOutput("TU",width = 4))),
+      tabItem(tabName = "TU1",fluidRow(infoBoxOutput("Tu1",width = 4))),
+      tabItem(tabName = "TU2",fluidRow(infoBoxOutput("TU2",width = 4))),
+      tabItem(tabName = "TU3",fluidRow(infoBoxOutput("Tu1",width = 4))),
+      tabItem(tabName = "TU4",fluidRow(infoBoxOutput("TU2",width = 4)))
+      
+     
+      
+      )
+        
+    )
+ )
+
+ )
